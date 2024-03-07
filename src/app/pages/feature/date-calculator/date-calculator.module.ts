6188e9c4b50bada56bbe52ab52d57d8fd18e6a2e
Ricardo MaGuz <maguz.ricardo@gmail.com>
2024-03-07 01:14:59 -0600

    Added date calculator component and module

---
+++ src/app/pages/feature/date-calculator/date-calculator.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DateCalculatorComponent } from './date-calculator.component';
+
+
+
+@NgModule({
+  declarations: [
+    DateCalculatorComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DateCalculatorModule { }
