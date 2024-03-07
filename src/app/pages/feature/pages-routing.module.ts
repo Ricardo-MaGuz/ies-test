20c88447d0cdce952f0978e6c9a335201d7410a9
Ricardo MaGuz <maguz.ricardo@gmail.com>
2024-03-07 14:30:35 -0600

    Added Shared Nav component

--- src/app/pages/feature/pages-routing.module.ts
+++ src/app/pages/feature/pages-routing.module.ts
@@ -4,27 +4,37 @@ import { HomeComponent } from './home/home.component';
 import { ConversionsComponent } from './conversions/conversions.component';
 import { FormComponent } from './form/form.component';
 import { DateCalculatorComponent } from './date-calculator/date-calculator.component';
+import { PagesComponent } from './pages.component';
 
 const routes: Routes = [
     {
-        path: 'conversions',
-        component: ConversionsComponent
-    },
-    {
-        path: 'form',
-        component: FormComponent
-    },
-    {
-        path: 'date-calculator',
-        component: DateCalculatorComponent
-    },
-    {
         path: '',
-        component: HomeComponent
+        component: PagesComponent,
+        children: [{
+            path: 'conversions',
+            component: ConversionsComponent
+        },
+        {
+            path: 'form',
+            component: FormComponent
+        },
+        {
+            path: 'date-calculator',
+            component: DateCalculatorComponent
+        }
+            ,
+        {
+            path: 'home',
+            component: HomeComponent
+        },]
+    },
+    {
+        path: '**',
+        redirectTo: 'home'
     }
 ];
 @NgModule({
     imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule],
+    exports: [RouterModule]
 })
 export class PagesoutingModule { }
--- src/app/pages/feature/pages.module.ts
+++ src/app/pages/feature/pages.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesComponent } from './pages.component';
 import { PagesoutingModule } from './pages-routing.module';
+import { NavModule } from 'src/app/shared/ui/nav/nav.module';
 
 
 
 @NgModule({
   declarations: [
     PagesComponent
+
   ],
   imports: [
     CommonModule,
-    PagesoutingModule
+    PagesoutingModule,
+    NavModule
   ]
 })
 export class PagesModule { }
