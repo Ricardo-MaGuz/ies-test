10c5db792f83fb203c653dcec881844c36b5e0f0
Ricardo MaGuz <maguz.ricardo@gmail.com>
2024-03-08 19:01:52 -0600

    Add app module moment config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatNativeDateModule } from '@angular/material/core';
+import {
+  MAT_MOMENT_DATE_FORMATS,
+  MomentDateAdapter,
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+} from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import 'moment/locale/mx';
 
 
 
@@ -17,12 +23,13 @@ import { MatNativeDateModule } from '@angular/material/core';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MatNativeDateModule,
-
-
-
   ],
-  providers: [],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'es-MX' }, {
+    provide: DateAdapter,
+    useClass: MomentDateAdapter,
+    deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+  },
+  { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
